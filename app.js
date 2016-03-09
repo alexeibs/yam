2d011e08a0c9f970c4d4e3e7ab592ee964d6cccc
Yuri Artemev <i@artemeff.com>
2016-03-09 22:38:22 +0300

    fix code styles

--- app.js
+++ app.js
@@ -2,22 +2,18 @@ const electron = require('electron');
 const ipc = electron.ipcRenderer;
 const embed = document.getElementById('embed');
 
-ipc.on('media-next-track', function () {
-    embed.executeJavaScript('externalAPI.next()');
+ipc.on('media-next-track', function() {
+  embed.executeJavaScript('externalAPI.next()');
 });
 
-ipc.on('media-prev-track', function () {
-    embed.executeJavaScript('externalAPI.prev()');
+ipc.on('media-prev-track', function() {
+  embed.executeJavaScript('externalAPI.prev()');
 });
 
-ipc.on('media-stop', function () {
-    embed.executeJavaScript('externalAPI.stop()');
+ipc.on('media-stop', function() {
+  embed.executeJavaScript('externalAPI.stop()');
 });
 
-ipc.on('media-play-pause', function () {
-    embed.executeJavaScript('externalAPI.togglePause()');
-});
-
-document.addEventListener('keydown', function(event) {
-    console.log(event.keyCode);
+ipc.on('media-play-pause', function() {
+  embed.executeJavaScript('externalAPI.togglePause()');
 });
